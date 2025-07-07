4640d6b1a2740c11547cd147d1c4d30e3188bf19
adeshkumar256 <adeshkumar256@gmail.com>
2025-07-07 19:11:15 +0530

    MOdels and data

--- my_site/blogs/models.py
+++ my_site/blogs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=500)
+    # null True if it can have null value or not
+    author = models.CharField(null=True, max_length=100)
+    # id = models.AutoField() no need as model will automatically create
+
+    def get_absolute_url(self):
+        return reverse("post-details", args=[self.id])
+
+    def __str__(self):
+        return f"{self.title} {self.rating} {self.description}"
--- my_site/blogs/views.py
+++ my_site/blogs/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    return render(request, "blogs/index.html")
+    posts = Post.objects.filter()
+    return render(request, "blogs/index.html", {
+        "posts": posts
+    })
 
 
 def get_posts(request):
@@ -11,6 +15,9 @@ def get_posts(request):
 
 
 def get_post(request, slug):
+    # post = Post.objects.get(pk=slug)
+    post = get_object_or_404(Post, pk=slug)
     return render(request, "blogs/post.html", {
-        "slug": slug
+        "slug": slug,
+        "post": post
     })
