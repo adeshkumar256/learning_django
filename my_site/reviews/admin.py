d4a7afea2a8d423114b8489a2a6d9a7ea5256c72
adeshkumar256 <adeshkumar256@gmail.com>
2025-07-14 19:35:56 +0530

    added review model amnd admin config for that

--- my_site/reviews/admin.py
+++ my_site/reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Review
 # Register your models here.
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "email", "rating")
+
+
+admin.site.register(Review, ReviewAdmin)
--- my_site/reviews/models.py
+++ my_site/reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MinLengthValidator, MaxValueValidator, MaxLengthValidator
 
 # Create your models here.
+
+
+class Review(models.Model):
+    user_name = models.CharField(
+        null=False,
+        blank=False,
+        max_length=100,
+        validators=[MinLengthValidator(8), MaxLengthValidator(100)])
+    email = models.EmailField(null=False)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
